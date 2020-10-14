0be4664f43f45782f9552c43f090688066ee43f4
alejandrofernandez312 <71879613+alejandrofernandez312@users.noreply.github.com>
2020-10-14 10:49:09 +0200

    Create Ecuacion.java

---
+++ src/first/Ecuacion.java
@@ -0,0 +1,22 @@
+package first;
+
+import java.util.Scanner;
+
+public class Ecuacion {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Teclee un valor para a:");
+		double a = input.nextDouble();
+		System.out.println("Teclee un valor para b:");
+		double b = input.nextDouble();
+		System.out.println("Teclee un valor para c:");
+		double c = input.nextDouble();
+		
+		
+
+	}
+
+}
