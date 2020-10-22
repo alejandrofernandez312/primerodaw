e8f862534e0bdd33c62d3076736a8c8cc26925f6
alejandrofernandez312 <71879613+alejandrofernandez312@users.noreply.github.com>
2020-10-22 08:59:57 +0200

    Update AdvancedMath.java

--- src/first/AdvancedMath.java
+++ src/first/AdvancedMath.java
@@ -8,32 +8,42 @@ public class AdvancedMath {
 		
 		Scanner input = new Scanner (System.in);
 		
-		
-		
 		int cont = 0;
+		int bien = 0;
 		while (cont<5) {
-			int n1 = (int)Math.random()*10;
-			int n2 = (int)Math.random()*10;
+			int n1 = (int)(Math.random()*10);
+			int n2 = (int)(Math.random()*10);
 			int answer = n1 - n2;
 		
 			if (n1>n2) {
 			System.out.println("¿Cuánto es " + n1 + "-" + n2 + "?");
 			int answer2 = input.nextInt();
-				if (answer2 != answer) {
-					int bien = cont -1;
+				if (answer2 == answer) {
+					System.out.println("¡Has acertado!");
+					++bien;
+				}
+				else {
+					System.out.println("Has fallado, la respuesta correcta es " + answer);
+				}
 				}
 			else{
 				System.out.println("¿Cuánto es " + n2 + "-" + n1 + "?");
 				answer = n2-n1;
-				answer2 = input.nextInt();
-					if (answer2 != answer) {
-						int bien = cont -1;
-					}
-			}
+				int answer2 = input.nextInt();
+				if (answer2 == answer) {
+					System.out.println("¡Has acertado!");
+					++bien;
+				}
+				else {
+					System.out.println("Has fallado, la respuesta correcta es " + answer);
+				}
 			}
 			cont++;
+			
+			}
+		
+		System.out.println("Has acertado " + bien + " de " + cont);
 		}
 
 	}
 
-}
