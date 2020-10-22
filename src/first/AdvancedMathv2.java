14dc9cf16b6a5e7607491654836c887c81ce3827
alejandrofernandez312 <71879613+alejandrofernandez312@users.noreply.github.com>
2020-10-22 13:02:32 +0200

    Update AdvancedMathv2.java

--- src/first/AdvancedMathv2.java
+++ src/first/AdvancedMathv2.java
@@ -13,55 +13,33 @@ public class AdvancedMathv2 {
 		while (cont<5) {
 			int n1 = (int)(Math.random()*10);
 			int n2 = (int)(Math.random()*10);
-			int answer = n1 - n2;
+		
 		
 			if (n1>n2) {
+			int answer = n1 - n2;
 			System.out.println("¿Cuánto es " + n1 + "-" + n2 + "?");
 			int answer2 = input.nextInt();
-				if (answer2 == answer) {
-					System.out.println("¡Has acertado!");
-					++bien;
-				}
-				
-				else {
-					while (answer2 != answer) {
-						System.out.println("Has fallado, vuelve a responder");
-						if (answer2 == answer) {
-							System.out.println("¡Has acertado!");
-						}
-			else{
-				System.out.println("¿Cuánto es " + n2 + "-" + n1 + "?");
-				answer = n2-n1;
-			    answer2 = input.nextInt();
-				if (answer2 == answer) {
-					System.out.println("¡Has acertado!");
-					++bien;
-				}
-				else {
-					while (answer2 != answer) {
-						System.out.println("Has fallado, vuelve a responder");
-						if (answer2 == answer) {
-							System.out.println("¡Has acertado!");				
-							
-						}
-					else{
-						System.out.println("¿Cuánto es " + n2 + "-" + n1 + "?");
-						answer = n2-n1;
-					    answer2 = input.nextInt();
-						if (answer2 == answer) {
-							System.out.println("¡Has acertado!");
-							
-						}
-					}
-				}
+			while (answer2 != answer) {
+				System.out.println("Respuesta incorrecta, vuelve a responder");
+				System.out.println("¿Cuánto es " + n1 + "-" + n2 + "?");
+				answer2 = input.nextInt();
 			}
+			System.out.println("Has acertado");
 			cont++;
 			
 			}
-					}
-				}
+			else {
+			System.out.println("¿Cuánto es " + n2 + "-" + n1 + "?");
+			int answer = n2 - n1;
+			int answer2 = input.nextInt();
+			while (answer2 != answer) {
+				System.out.println("Respuesta incorrecta, vuelve a responder");
+				System.out.println("¿Cuánto es " + n2 + "-" + n1 + "?");
+				answer2 = input.nextInt();
 			}
+			System.out.println("Has acertado");
+			cont++;
 		}
 	}
 }
-		
+}
