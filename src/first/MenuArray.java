090dfa48125389f169ef04b10e42c5dbd4ea17c1
alejandrofernandez312 <71879613+alejandrofernandez312@users.noreply.github.com>
2020-11-16 21:10:26 +0100

    Update MenuArray.java

--- src/first/MenuArray.java
+++ src/first/MenuArray.java
@@ -1,4 +1,4 @@
-package first;
+package First;
 
 import java.util.Scanner;
 
@@ -7,39 +7,56 @@ public class MenuArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
+
 		int op;
-		
+		String printarray = "";
+		String printarray1 = "";
+
 		do {
 			menu();
 			System.out.println();
 			System.out.println("Elige un número:");
-			op =input.nextInt();
-			
-			switch(op) {
+			op = input.nextInt();
+
+			switch (op) {
 			case 1:
 				System.out.println("¿Cuántos valores quieres que tenga el array?");
 				int n = input.nextInt();
-				int [] array= new int[n];
-				for (int i=0;i<n;i++) {
-					System.out.println("Teclea un valor para la celda "+i+":");
-					int valor=input.nextInt();
-					array[i]=valor;
-					i++;
+				int[] array = new int[n];
+				for (int i = 0; i < n; i++) {
+					System.out.println("Teclea un valor para la celda " + i + ":");
+					int valor = input.nextInt();
+					array[i] = valor;
+				}
+				for (int i = 0; i < n; i++) {
+					printarray += " " + array[i];
 				}
 				break;
 			case 2:
-				System.out.println("El array va a tener un número random de valores del 1 al 10.");
-				int c=(int)(Math.random()*10)+1;
-				int [] array2= new int[c];
-				System.out.println("En este caso el array tiene "+c+" valores.");
+				System.out.println("¿Cuántos valores quieres que tenga el array?");
+				n = input.nextInt();
+				int[] miarray = new int[n];
+				for (int i = 0; i < n; i++) {
+					int valor = (int) (Math.random() * 10) + 1;
+					miarray[i] = valor;
+					i++;
+				}
+				for (int i = 0; i < n; i++) {
+					printarray1 += " " + miarray[i];
+				}
+				break;
+			case 3:
+				System.out.println(printarray);
+				System.out.println(printarray1);
+				break;
+			case 4:
+				
 			}
-			
-			
-		}while(op!=9);
+
+		} while (op != 9);
 
 	}
-	
+
 	public static void menu() {
 		System.out.println("\t\tMenu");
 		System.out.println("-------------------------------------");
@@ -51,7 +68,8 @@ public class MenuArray {
 		System.out.println("\t6.-Finding the smallest index of the largest element");
 		System.out.println("\t7.-Random shuffling");
 		System.out.println("\t8.-Shifting elements");
+		System.out.println("\t9.-Exit");
 	}
-	
 
+	
 }
