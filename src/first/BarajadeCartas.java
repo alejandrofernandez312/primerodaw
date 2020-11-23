cab91595241f796ec64e3542baaef4f4b502552f
alejandrofernandez312 <71879613+alejandrofernandez312@users.noreply.github.com>
2020-11-23 19:23:26 +0100

    Update BarajadeCartas.java

--- src/first/BarajadeCartas.java
+++ src/first/BarajadeCartas.java
@@ -1,4 +1,4 @@
-package first;
+package First;
 
 import java.util.Scanner;
 
@@ -6,58 +6,77 @@ public class BarajadeCartas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] barajanueva = nuevabaraja();	
-		Scanner input = new Scanner (System.in);
-		String [] barajaconpalos=barajaconpalos(barajanueva);
+		int[] barajanueva = nuevabaraja();
+		Scanner input = new Scanner(System.in);
+		String[] barajaconpalos = barajaconpalos(barajanueva);
+		String[] barajabarajada = barajarbaraja(barajaconpalos(barajanueva));
+
 		int op;
-		
+
 		do {
 			menu();
 			System.out.println("Elige un número:");
-			op=input.nextInt();
+			op = input.nextInt();
 			switch (op) {
 			case 1:
 				System.out.println("Estos son los números ordenados");
 				System.out.println();
 				printarray(barajanueva);
 				break;
-			case 2: 
+			case 2:
 				System.out.println("Esta es la baraja ordenada");
 				System.out.println();
 				printbaraja(barajaconpalos);
-				
+				break;
+			case 3:
+				System.out.println("Esta es la baraja barajada");
+				System.out.println();
+				printbaraja(barajabarajada);
+				break;
 			}
-		}while(op!=3);
+		} while (op != 4);
 
 	}
-	
-	
-	public static String [] barajaconpalos(int []barajanueva) {
-		String [] resultado=new String [40];
-		int b=0;
-		int c=0;
-		int e=0;
-		for (int i=0;i<barajanueva.length;i++) {
-			if (barajanueva[i]<=10) {
-				resultado[i]=barajanueva[i]+" de oros";
-			}else if (barajanueva[i]<=20) {
-				resultado[i]=barajanueva[b]+" de basto";
+
+	public static String[] barajarbaraja(String[] array) {
+		String[] barajabarajada = new String[40];
+		for (int i = 0; i < array.length; i++) {
+			int r = (int) (Math.random() * array.length);
+			String aux = array[i];
+			array[i] = array[r];
+			array[r] = aux;
+		}
+		for (int i = 0; i < array.length; i++)
+			barajabarajada[i] = array[i];
+
+		return barajabarajada;
+	}
+
+	public static String[] barajaconpalos(int[] barajanueva) {
+		String[] resultado = new String[40];
+		int b = 0;
+		int c = 0;
+		int e = 0;
+		for (int i = 0; i < barajanueva.length; i++) {
+			if (barajanueva[i] <= 10) {
+				resultado[i] = barajanueva[i] + " de oros";
+			} else if (barajanueva[i] <= 20) {
+				resultado[i] = barajanueva[b] + " de basto";
 				b++;
-			}else if (barajanueva[i]<=30) {
-				resultado[i]=barajanueva[c]+" de copas";
+			} else if (barajanueva[i] <= 30) {
+				resultado[i] = barajanueva[c] + " de copas";
 				c++;
-			}else if (barajanueva[i]<=40) {
-				resultado[i]=barajanueva[e]+" de espadas";
+			} else if (barajanueva[i] <= 40) {
+				resultado[i] = barajanueva[e] + " de espadas";
 				e++;
 			}
 		}
 		return resultado;
-		
+
 	}
-	
 
 	public static int[] nuevabaraja() {
-		int [] baraja= new int [40];
+		int[] baraja = new int[40];
 		int z = 1;
 		for (int i = 0; i < baraja.length; i++) {
 			baraja[i] = z;
@@ -77,7 +96,7 @@ public class BarajadeCartas {
 		System.out.println();
 		System.out.println();
 	}
-	
+
 	public static void printbaraja(String[] array) {
 		for (int i = 0; i < array.length; i++) {
 			if (i % 10 != 0)
@@ -93,5 +112,7 @@ public class BarajadeCartas {
 	public static void menu() {
 		System.out.println("1.-Ver números ordenados");
 		System.out.println("2.-Ver baraja ordenada");
+		System.out.println("3.-Ver baraja barajada");
+		System.out.println("4.-Salir");
 	}
 }
